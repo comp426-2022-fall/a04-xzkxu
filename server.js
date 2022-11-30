52aeb8706e665e7cbff00a16f3805a1672eebc09
Zhaokun Xu <zhaokunxu@ZhaokundeMBP.lan>
2022-11-30 00:01:17 -0500

    abc

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ import minimist from "minimist"
 import express from "express"
 
 const app = express();
-const args = minimist((process.argv.slice(2)));
+const args = minimist(process.argv.slice(2));
 
 app.use(express.urlencoded({extended: true}));
 
-app.get('/app', (req, res) => {
+app.get('/app/', (req, res) => {
     res.send("200 OK");
 });
 
-app.get('/app/roll', (req, res) => {
+app.get('/app/roll/', (req, res) => {
     res.send(roll(6, 2, 1));
 });
 
@@ -31,8 +31,8 @@ app.get('/app/roll/:sides/:dice/:rolls', (req, res) => {
     res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls)));
 });
 
-app.use((req, res) => {
-    res.status(404).send("404 NOT FOUND")
-});
+app.get('/app/*', (req, res) => {
+    res.send("404 NOT FOUND");
+})
 
 app.listen(args.port || 5000);
