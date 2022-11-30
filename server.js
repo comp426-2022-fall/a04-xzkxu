5efa6e67fc4458f31fae4a5dc98758f0a00c013b
xzkxu <zhaokun@ad.unc.edu>
2022-11-30 16:13:12 -0500

    roll random dice

--- server.js
+++ server.js
@@ -15,25 +15,27 @@ app.get('/app/', (req, res) => {
 });
 
 app.get('/app/roll/', (req, res) => {
-    res.send(roll(6, 2, 1));
-});
-
-app.get('/app/roll/', (req, res) => {
-    res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls)));
+    var sides = 6;
+    var dice = 2;
+    var rolls = 1;
+    if(req.body.sides) {sides = req.body.sides;}
+    if(req.body.dice) {sides = req.body.dice;}
+    if(req.body.rolls) {sides = req.body.rolls;}
+    res.send(roll(sides, dice, rolls));
 });
 
 app.get('/app/roll/:sides/', (req, res) => {
-    res.send(roll(parseInt(req.params.sides), 2, 1));
+    res.send(roll(parseInt(req.body.sides), 2, 1));
     res.end();
 });
 
 app.get('/app/roll/:sides/:dice/', (req, res) => {
-    res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), 1));
+    res.send(roll(parseInt(req.body.sides), parseInt(req.body.dice), 1));
     res.end();
 });
 
 app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
-    res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls)));
+    res.send(roll(parseInt(req.body.sides), parseInt(req.body.dice), parseInt(req.body.rolls)));
     res.end();
 });
 
