0ff21aaa3300492323634f7e1afbcb6f5714e7d2
Zhaokun Xu <zhaokunxu@zhaokundembp.wireless-1x.unc.edu>
2022-11-30 13:19:59 -0500

    a

--- server.js
+++ server.js
@@ -11,30 +11,37 @@ app.use(express.urlencoded({extended: true}));
 
 app.get('/app/', (req, res) => {
     res.send("200 OK");
+    res.end();
 });
 
 app.get('/app/roll/', (req, res) => {
     res.send(roll(6, 2, 1));
+    res.end();
 });
 
 app.get('/app/roll/', (req, res) => {
     res.send(roll(parseInt(req.body.sides), parseInt(req.body.dice), parseInt(req.body.rolls)));
+    res.end();
 });
 
 app.get('/app/roll/:sides/', (req, res) => {
     res.send(roll(parseInt(req.params.sides), 2, 1));
+    res.end();
 });
 
 app.get('/app/roll/:sides/:dice/', (req, res) => {
     res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), 1));
+    res.end();
 });
 
 app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
     res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls)));
+    res.end();
 });
 
 app.use((req, res) => {
-    res.status(404).send("404 NOT FOUND")
+    res.status(404).send("404 NOT FOUND");
+    res.end();
 })
 
-app.listen(args.port || 5000);
+console.log(app.listen(args.port || 5000));
